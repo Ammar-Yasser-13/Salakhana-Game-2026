335717f9ed39da39011f4cdf47f6b7ee4e07ff24
M.K.S <mknights524@gmail.com>
2026-02-06 17:33:05 +0200

    added medkit and bullets pickups

---
+++ twin-edge/Scripts/pick ups/medkit.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+@onready var root_scene: Node3D = $"Root Scene"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	root_scene.rotate_y(0.02)
+
+
+func _on_body_entered(_body: Node3D) -> void:
+	print("+1 HP")
+	queue_free()
