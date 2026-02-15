fb1267c576bcc99d5411ffdbb402a9c3f981a868
M.K.S <mknights524@gmail.com>
2026-02-15 21:46:46 +0200

    ordered project files

--- twin-edge/Scripts/Menus/main_menu.gd
+++ twin-edge/Scripts/Menus/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 @onready var tutorial_popup = $"tutorial panel"
 func _on_level_select_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_select.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menus/level_select.tscn")
 
 
 func _on_toturial_pressed() -> void:
