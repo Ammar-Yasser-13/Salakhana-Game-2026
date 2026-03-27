3ec966656746ea58b33144f6687d50c85995c0ce
M.K.S <mknights524@gmail.com>
2026-03-27 05:21:12 +0200

    updated backup enemy with: states and bullet detection

--- twin-edge/Dummy/Dummy Scripts/backup_enemy.gd
+++ twin-edge/Dummy/Dummy Scripts/backup_enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
-#====={Enemy stats}=====> 
-var player_visablity:bool = false
+#====={Enemy variables}=====> 
+var player_visibility:bool = false
 var player_position:Vector3
 var Player:Node3D = null
 @export var Enemy_health:int = 1
@@ -11,6 +11,7 @@ func _physics_process(delta: float) -> void:
 	if Player:
 		player_position = Player.global_position
 	Enemy_rotate(delta)
+	Enemy_State_checker()
 	move_and_slide()
 
 #====={Enemy detect player}=====>
@@ -33,4 +34,27 @@ func Enemy_rotate(delta) -> void:
 	var new_angle = lerp_angle(current_angle, target_angle, Enemy_rotation_speed * delta)
 	rotation.y = new_angle
 
-#====={Enemy undetect player}=====>
+#====={Enemy states}=====>
+func Enemy_State_checker()->void:
+#==> idle state
+	if Enemy_health == 1 && Player == null:
+		pass
+		#print("idle")
+#==> death state
+	if Enemy_health < 1:
+		pass
+#==> fighting state
+	if Enemy_health == 1 && Player:
+		pass
+		#print("fighting")
+
+#====={Enemy damage taking}=====>
+func Enemy_take_damage()-> void:
+	Enemy_health -= 1
+	print("enemy health " + str(Enemy_health))
+
+#====={Enemy hurtbox}=====>
+func bullet_hit(body: Node3D) -> void:
+	#print("damaged")
+	if body.is_in_group("bullet") :
+		Enemy_take_damage()
