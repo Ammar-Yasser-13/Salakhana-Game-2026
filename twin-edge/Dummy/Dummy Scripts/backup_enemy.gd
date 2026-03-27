8771a906da8ce099ecdf81da7b3db67eed38d197
M.K.S <mknights524@gmail.com>
2026-03-27 08:04:53 +0200

    added shooting and adjustable vars for backup enemy

--- twin-edge/Dummy/Dummy Scripts/backup_enemy.gd
+++ twin-edge/Dummy/Dummy Scripts/backup_enemy.gd
@@ -4,14 +4,25 @@ extends CharacterBody3D
 var player_visibility:bool = false
 var player_position:Vector3
 var Player:Node3D = null
+@export_category("Enemy adjustable variables")
 @export var Enemy_health:int = 1
 @export var Enemy_rotation_speed:float = 5.3
+@export var bullet_speed:float = 20
+#====={Enemy reffrences}=====> 
+@onready var Bullet_Scene = preload("res://scenes/pick ups/bullet_projectile.tscn")
+@onready var marker_3d: Marker3D = $Marker3D
+@onready var cooldown: Timer = $cooldown
+#====={Enemy States}=====>
+var Idle:bool = true
+var dead:bool = false
+var fighting:bool = false
 
 func _physics_process(delta: float) -> void:
 	if Player:
 		player_position = Player.global_position
 	Enemy_rotate(delta)
 	Enemy_State_checker()
+	can_shoot()
 	move_and_slide()
 
 #====={Enemy detect player}=====>
@@ -19,6 +30,7 @@ func player_detect(body: Node3D) -> void:
 	if body.is_in_group("Player"):
 		print("player entered")
 		Player = body
+		fighting = true
 
 #====={Enemy undetect player}=====>
 func Player_detect_exited(body: Node3D) -> void:
@@ -26,7 +38,7 @@ func Player_detect_exited(body: Node3D) -> void:
 		print("player exited")
 		Player = null
 
-#====={Enemy undetect player}=====>
+#====={Enemy handle rotaition}=====>
 func Enemy_rotate(delta) -> void:
 	var direction = (player_position - global_position).normalized()
 	var target_angle = atan2(direction.x, direction.z)
@@ -38,14 +50,21 @@ func Enemy_rotate(delta) -> void:
 func Enemy_State_checker()->void:
 #==> idle state
 	if Enemy_health == 1 && Player == null:
-		pass
+		Idle = true
+		dead = false
+		fighting = false
 		#print("idle")
 #==> death state
 	if Enemy_health < 1:
-		pass
+		Idle = false
+		dead = true
+		fighting = false
+		#print("dead")
 #==> fighting state
 	if Enemy_health == 1 && Player:
-		pass
+		Idle = false
+		dead = false
+		fighting = true
 		#print("fighting")
 
 #====={Enemy damage taking}=====>
@@ -58,3 +77,23 @@ func bullet_hit(body: Node3D) -> void:
 	#print("damaged")
 	if body.is_in_group("bullet") :
 		Enemy_take_damage()
+
+#====={Enemy shooting setup player}=====>
+func shoot_player()->void:
+
+	var bullet = Bullet_Scene.instantiate()
+	get_tree().current_scene.add_child(bullet)
+	#==> positioning bullet
+	if marker_3d:
+		bullet.global_position = marker_3d.global_position
+	else:
+		bullet.global_position =  global_position + -transform.basis.z * 1.5
+	#==> bullet speed and direction
+	var direction_to_player = (Player.global_position - bullet.global_position).normalized()
+	bullet.velocity = direction_to_player * bullet_speed
+
+#====={Enemy shooting player}=====>
+func can_shoot()->void:
+	if fighting == true:
+		shoot_player()
+		cooldown.start()
