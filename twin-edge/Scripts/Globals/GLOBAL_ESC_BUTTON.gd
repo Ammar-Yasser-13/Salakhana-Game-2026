78c81cd0117e4ae721e8d929083cd11669fc3357
Ammar-Yasser-13 <ammaryasser2005269@gmail.com>
2026-04-11 23:27:39 +0200

    Changes to ui

--- twin-edge/Scripts/Globals/GLOBAL_ESC_BUTTON.gd
+++ twin-edge/Scripts/Globals/GLOBAL_ESC_BUTTON.gd
@@ -3,3 +3,4 @@ extends Node
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		get_tree().change_scene_to_file("res://scenes/Menus/main_menu.tscn")
